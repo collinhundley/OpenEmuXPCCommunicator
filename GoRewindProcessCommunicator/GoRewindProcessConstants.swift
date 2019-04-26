fdd8737fc3e4f38bfeb306e41260a803cab474fe
Almin Milanovic <almin_bug@hotmail.com>
2019-04-26 12:28:23 +0200

    undo reverted changes

--- GoRewindProcessCommunicator/GoRewindProcessConstants.swift
+++ GoRewindProcessCommunicator/GoRewindProcessConstants.swift
@@ -12,13 +12,12 @@ public typealias ContextIdentifier = String
 
 public struct GoRewindProcessConstants {
     
-    // `M37GitBranch` key needs to be present in all targets
     private static var serviceNamePrefix: String {
-        if let branch = (Bundle.main.object(forInfoDictionaryKey: "M37GitBranch") as? String) {
-            return branch + "."
-        } else {
-            return ""
-        }
+        #if DEBUG
+        return "dev."
+        #else
+        return ""
+        #endif
     }
     
     public static var fullServiceName: String {
