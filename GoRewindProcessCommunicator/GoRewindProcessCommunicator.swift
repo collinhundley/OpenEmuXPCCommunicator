f272c886fb69a2f5f251a553cb1c6d2d78141ca0
Konstantin Gonikman <info@alstertouch.de>
2019-05-01 13:01:01 +0200

    – Fix: start agent as an argument (should be done from the main app only) – Fix: reading plist path should be done from the caller

--- GoRewindProcessCommunicator/GoRewindProcessCommunicator.swift
+++ GoRewindProcessCommunicator/GoRewindProcessCommunicator.swift
@@ -9,6 +9,42 @@
 import Foundation
 
 public class GoRewindProcessCommunicator {
+    
+    private static func infoPlist(plistUrl: URL) -> Dictionary<String, AnyObject>? {    
+        guard FileManager.default.fileExists(atPath: plistUrl.path) else { return nil }
+        
+        if let pListData = FileManager.default.contents(atPath: plistUrl.path) {
+            do {
+                let pListObject = try PropertyListSerialization.propertyList(from: pListData, options:PropertyListSerialization.ReadOptions(), format:nil)
+                
+                guard let pListDict = pListObject as? Dictionary<String, AnyObject> else {
+                    return nil
+                }
+                
+                return pListDict
+                
+            } catch {
+                print("Error reading regions plist file: \(error)")
+                return nil
+            }
+        }
+        
+        return nil
+    }
+    
+    public static func setupConnection(with plistUrl: URL, startAgent: Bool) {
+        if let plist = infoPlist(plistUrl: plistUrl), let branch = plist["M37GitBranch"] as? String {
+            GoRewindProcessConstants.serviceNamePrefix = branch + "."
+        }
+        
+        if startAgent {
+            OEXPCCAgentConfiguration.defaultConfiguration(withName: GoRewindProcessConstants.serviceName)
+        }
+        
+        OEXPCCAgent.defaultAgent(withServiceName: GoRewindProcessConstants.fullServiceName)
+    }
+    
+    
     public static func terminate() {
         OEXPCCAgentConfiguration.current()?.tearDownAgent()
     }
--- GoRewindProcessCommunicator/GoRewindProcessConstants.swift
+++ GoRewindProcessCommunicator/GoRewindProcessConstants.swift
@@ -11,39 +11,9 @@ import Foundation
 public typealias ContextIdentifier = String
 
 public struct GoRewindProcessConstants {
-    
-    private static func infoPlist() -> Dictionary<String, AnyObject>? {    
-        let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath, isDirectory: true).appendingPathComponent("../../../../Info.plist")
-        
-        guard FileManager.default.fileExists(atPath: url.path) else { return nil }
-        
-        if let pListData = FileManager.default.contents(atPath: url.path) {
-            do {
-                let pListObject = try PropertyListSerialization.propertyList(from: pListData, options:PropertyListSerialization.ReadOptions(), format:nil)
-                
-                guard let pListDict = pListObject as? Dictionary<String, AnyObject> else {
-                    return nil
-                }
-                
-                return pListDict
-                
-            } catch {
-                print("Error reading regions plist file: \(error)")
-                return nil
-            }
-        }
-        
-        return nil
-    }
-    
-    private static var serviceNamePrefix: String {        
-        if let plist = infoPlist(), let branch = plist["M37GitBranch"] as? String {
-            return branch + "."
-        } else {
-            return ""
-        }
-    }
-    
+
+    internal static var serviceNamePrefix: String  = ""      
+   
     public static var fullServiceName: String {
         return "ai.m37.GoRewind.OEXPCCAgent." + serviceName
     }
